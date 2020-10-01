3a53f5f5634afb41319014335ebf12fa1f6799b5
Catriel Müller <catrielmuller@gmail.com>
2020-10-01 17:34:23 -0300

    Changed empty string to null to be more representative

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const App = () => {
   const technologies = convertDataToTechnologies(technologyData, locations);
   const [showAboutPage, setShowAboutPage] = useState(false);
   const [highlightedTechnology, setHighlightedTechnology] = useState(
-    technologies.length ? technologies[0].name : ''
+    technologies.length ? technologies[0].name : null
   );
 
   const onClickLocationCheckbox = locationName => {
