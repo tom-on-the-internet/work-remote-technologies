6ec06e04fcac416af4bf38091194678950a15a33
Thomas Steven <thomaspatricksteven@gmail.com>
2021-02-03 20:08:09 -0500

    update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default () => {
   return (
     <footer>
-      <div>Data from September 24, 2020</div>
+      <div>Data from February 03, 2021</div>
       <a href="https://tomontheinternet.com">tomontheinternet.com</a>
     </footer>
   );
